cc0da22fe1a5863d9513c5391f76c13fc3bb8d91
Christine Hoang <christinehoangt@gmail.com>
2023-12-08 20:24:18 -0600

    save

--- answers.sql
+++ answers.sql
@@ -6,13 +6,13 @@ SELECT * FROM customers ORDER BY email asc;
 --#2
 SELECT * FROM customers JOIN orders ON customers.id=orders.customer_id WHERE customers.fname LIKE '%Elizabeth%' AND customers.lname LIKE '%Crocker%';
 
---# 3 Something is wrong with this query. Returning 256 instead of 209.
-SELECT SUM (num_cupcakes) FROM orders WHERE orders.processed='t';
+--#3
+SELECT SUM (num_cupcakes) FROM orders WHERE processed='f';
 
 --#4
 SELECT name, SUM (num_cupcakes) FROM cupcakes LEFT JOIN orders ON orders.cupcake_id = cupcakes.id GROUP BY cupcakes.name;
 
---5
+--#5
 SELECT SUM (orders.num_cupcakes) AS Total, customers.fname,customers.lname, customers.email FROM customers JOIN orders ON orders.customer_id = customers.id GROUP BY customers.fname, customers.lname, customers.email;
 
 --#6
